c4f40c63bfd185198feb38c5e1aec1ddf2f9f57d
18016080 <18016080@myrp.edu.sg>
2020-07-24 11:06:47 +0800

    Done

--- app/src/main/java/sg/edu/rp/c346/demoswiper/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demoswiper/MainActivity.java
@@ -1,6 +1,9 @@
 package sg.edu.rp.c346.demoswiper;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -13,13 +16,16 @@ public class MainActivity extends AppCompatActivity {
     ArrayList <Fragment> al;
     MyFragmentPagerAdapter adapter;
     ViewPager vPager;
-
+    Button btnBack, btnNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         vPager = findViewById(R.id.viewpager1);
+        btnBack = findViewById(R.id.btnBack);
+        btnNext = findViewById(R.id.btnNext);
+
 
         FragmentManager fm = getSupportFragmentManager();
 
@@ -30,6 +36,25 @@ public class MainActivity extends AppCompatActivity {
         adapter = new MyFragmentPagerAdapter(fm, al);
 
         vPager.setAdapter(adapter);
-    }
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (vPager.getCurrentItem() > 0){
+                    int previousPage = vPager.getCurrentItem() - 1;
+                    vPager.setCurrentItem(previousPage, true);
+                }
+            }
+        });
 
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int max = vPager.getChildCount();
+                if (vPager.getCurrentItem() < max-1){
+                    int nextPage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextPage, true);
+                }
+            }
+        });
+    }
 }
